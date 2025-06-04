c4b5826b99749d428e36ec944067ec3f0e361274
Wera Barinova <alisawera@gmail.com>
2025-06-04 21:19:12 +0300

    Получение числового значения и вычисления величин

---
+++ 20250604_Js_input/birthday.js
@@ -0,0 +1,6 @@
+function start() {
+    //console.log('С Днём Рождения!')
+    // <input type="number" id="year" placeholder="Год Рождения">
+    let age = 2025 - year.valueAsNumber;
+    console.log(age)
+}
