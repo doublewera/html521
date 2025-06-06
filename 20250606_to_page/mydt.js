d9b1f88fe7596721937743e1d3cc6dfde8d13d59
Wera Barinova <alisawera@gmail.com>
2025-06-06 21:27:30 +0300

    Работа с датой и временем

---
+++ 20250606_to_page/mydt.js
@@ -0,0 +1,5 @@
+let dt = new Date()  // Создать объект, хранящий текущую дату и время
+
+console.log('Сейчас ' + dt.getFullYear() +  '  год')
+console.log('Сегодня ' + dt.getDate() + ', ' + dt.getMonth())
+console.log('Сейчас ' + dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds())
