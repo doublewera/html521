9a811ba6e64d4bf793f5ffd5587c66f11b463c02
Wera Barinova <alisawera@gmail.com>
2025-06-25 20:50:06 +0300

    Добавили цикл для ряда ячеек от 1 до 9 Добавили заголовку таблицы атрибут colspan

--- 20250625_multi_table/multiscript.js
+++ 20250625_multi_table/multiscript.js
@@ -5,6 +5,7 @@ console.log('Скрипт подключен')
  * 3. Создать ячейку
  * 4. Прикрепить ячейку к РЯДУ
  * 5. В ячейке написать 2*2=4
+ * 6. Сделать ряд ячеек с числами от 1 до 9
  */
 // 1. Создать ряд
 let tr = document.createElement('tr')  // Внутри название элемента, который мы создаем
@@ -12,9 +13,16 @@ let tr = document.createElement('tr')  // Внутри название элем
 // КУДА присоединяем ЧТО
 multbl.appendChild(   tr)
 // к таблице по айдишнику присоединяем свежесозданный ряд
-// 3. Создать ячейку
-let td = document.createElement('td')
-// 4. Прикрепить ячейку к РЯДУ
-tr.appendChild(td)
-// 5. В ячейке написать 2*2=4
-td.textContent = '2*2=4'
+const max_num = 9
+header.setAttribute('colspan', max_num)
+let index = 1
+while (index <= max_num) {
+    // ПОВТОРЯТЬ, ПОКА НЕ ДОСТИГНЕМ 9
+    // 3. Создать ячейку
+    let td = document.createElement('td')
+    // 4. Прикрепить ячейку к РЯДУ
+    tr.appendChild(td)
+    // 5. В ячейке написать 1
+    td.textContent = index
+    index++
+}
