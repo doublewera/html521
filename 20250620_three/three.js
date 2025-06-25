cbd01ffff22e5dde3f3c4ae098b10f83c6d08150
Wera Barinova <alisawera@gmail.com>
2025-06-25 22:30:55 +0300

    Конспект о строках

--- 20250620_three/three.js
+++ 20250620_three/three.js
@@ -2,9 +2,9 @@
  * Вывести в консоль в столбик все натуральные числа,
  * которые меньше 100 и делятся нацело на 3: 3, 6, ... 99
  */
-let n = 3;
-const plus = n;
-while (n < 100) {
-    console.log(n)
-    n = n + plus  
+let chislo = 3;
+const step = n;
+while (chislo < 100) {
+    console.log(chislo)
+    chislo = chislo + step  
 }
---
+++ 20250625_arrays/arr_test.js
@@ -0,0 +1,15 @@
+const imya = 'Вася'
+//console.log(imya[1], imya.charCodeAt(1))
+//console.log(String.fromCharCode(97))
+// Буквы нумеруются с нуля
+// Задание: вывести все буквы имени
+let bukva_nomer = 0
+while (bukva_nomer < 4) {
+    console.log(imya[bukva_nomer])
+    bukva_nomer += 2
+}
+
+
+const myletter = 'B';
+console.log(myletter.charCodeAt(0))
+console.log(String.fromCharCode(97))
