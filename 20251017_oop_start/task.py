d4def7ccbf4ec6e9ac6df2298a0982497d3a4c43
Wera Barinova <alisawera@gmail.com>
2025-10-17 22:06:51 +0300

    Свои классы и методы

--- 20251017_oop_start/task.py
+++ 20251017_oop_start/task.py
@@ -7,14 +7,29 @@ class Task:
             dt_till=datetime.now() + timedelta(1)):
             # Необязательный параметр: если его не указать, 
             # будет выбрана дата сразу за созданием задачи
-        self.dt             = datetime.now()
-        self.deadline       = dt_till
-        self.description    = description
-        self.done           = False
+        self.dt           = datetime.now()  # Время постановки задачи
+        self.deadline     = dt_till         # Срок окончания работы
+        self.description  = description     # Описание (суть) задачи
+        self.done         = False           # Завершена ли задача?
 
     def later(self, more_days):
         self.deadline += timedelta(more_days)
 
+    # Делает задачу завершённой
+    def finish(self):  # в отличие от функций, у методов, если не предполагается 
+        # передача параметров извне, для работы с полями класса обязательно self
+        self.done = True
+
+    # Проверяет, завершена ли задача?
+    def is_done(self):
+        return self.done
+    # Устанавливает новое описание.
+    # Устанавливает новый дедлайн
+
+
 t = Task('Написать классы Автомобиль, Человек, Одежда, Товар')
 print(t.deadline)  # какой получился срок у задачи?
 t.later(3)         # отсрочить задачу на три дня. Какой теперь срок? Напечатайте
+print(t.is_done)   # Метод требуется вызвать, чтобы он работал. Так он напечатается "это метод" 
+print(t.finish())  # завершает задачу!
+print(t.is_done()) # Вызывает метод проверки, завершена ли задача
